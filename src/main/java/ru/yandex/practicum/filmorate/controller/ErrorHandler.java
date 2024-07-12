505a1e741ab90a1e7d28b21b317f6a949b58bf74
lazarx77 <lazarx77@yandex.ru>
2024-07-12 09:28:26 +0400

    fix/ ErrorHandler fix with handleThrowable

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -2,6 +2,7 @@ package ru.yandex.practicum.filmorate.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -16,7 +17,7 @@ import java.util.Map;
 @Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
-    @ExceptionHandler
+    @ExceptionHandler({MethodArgumentNotValidException.class, ValidationException.class})
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public Map<String, String> handleValidationException(final ValidationException e) {
         log.error("Ошибка валидации данных: {}.", e.getMessage());
@@ -32,6 +33,16 @@ public class ErrorHandler {
         log.error("Ошибка с входными параметрами: {}.", e.getMessage());
         return Map.of(
                 "error", "Ошибка с входными параметрами.",
+                "description", e.getMessage()
+        );
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public Map<String, String> handleThrowable(final Throwable e) {
+        log.error("Возникла ошибка: {}.", e.getMessage());
+        return Map.of(
+                "error", "Возникла ошибка.",
                 "description", e.getMessage()
         );
     }
