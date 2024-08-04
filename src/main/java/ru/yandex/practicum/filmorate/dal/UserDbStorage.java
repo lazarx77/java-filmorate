0dae2a72037fb491d0277095975ad98ebfa903ac
lazarx77 <lazarx77@yandex.ru>
2024-08-04 10:21:16 +0400

    fix/ UserDbStorage updated

--- src/main/java/ru/yandex/practicum/filmorate/dal/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/UserDbStorage.java
@@ -25,8 +25,6 @@ public class UserDbStorage extends BaseRepository<User> implements UserStorage {
             "VALUES (?,?,?,?)";
     private static final String UPDATE_QUERY = "UPDATE USERS SET USER_NAME = ?, EMAIL = ?, LOGIN = ?, BIRTHDAY = ? " +
             "WHERE USER_ID = ?";
-//    private final Map<Long, User> users = null;
-
 
     private static final String FIND_FRIENDS_BY_ID = "SELECT FRIEND_ID AS USER_ID, EMAIL, LOGIN, USER_NAME, BIRTHDAY " +
             "FROM FRIENDSHIP INNER JOIN USERS ON FRIENDSHIP.FRIEND_ID = USERS.USER_ID WHERE FRIENDSHIP.USER_ID = ?";
@@ -62,33 +60,6 @@ public class UserDbStorage extends BaseRepository<User> implements UserStorage {
         return user;
     }
 
-
-//    private void checkUserFieldsOnCreate(User user) {
-//        log.info("Проверка полей пользователя при его создании; {}", user.getLogin());
-//        String FIND_BY_EMAIL = "SELECT * FROM USERS WHERE EMAIL =?";
-//        findOne(FIND_BY_EMAIL, user.getEmail());
-//        if (findOne(FIND_BY_EMAIL, user.getEmail()).isPresent()) {
-//            throw new ValidationException("Этот имейл " + user.getEmail() + " уже используется");
-//        }
-//        String FIND_BY_LOGIN = "SELECT * FROM USERS WHERE LOGIN = ?";
-//        if (findOne(FIND_BY_LOGIN, user.getLogin()).isPresent()) {
-//            throw new ValidationException("Пользователь с таким логином " + user.getLogin() + " уже существует.");
-//        }
-//    }
-
-//    private void checkUserFieldsOnUpdate(User updatedUser) {
-//        log.info("Проверка полей пользователя при его обновлении; {}", updatedUser.getLogin());
-//        if (findById(updatedUser.getId()).isEmpty()) {
-//            throw new NotFoundException("Польователь с id = " + updatedUser.getId() + " не найден");
-//        }
-//
-//        String sql = "SELECT * FROM USERS WHERE EMAIL = ? AND USER_ID != ?";
-//        if (findOne(sql, updatedUser.getEmail(), updatedUser.getId()).isPresent()) {
-//            throw new ValidationException("Этот имейл " + updatedUser.getEmail() + " уже используется");
-//            }
-//        }
-
-
     @Override
     public User update(User updatedUser) {
         log.info("Проверка наличия id пользователя в запросе: {}.", updatedUser.getLogin());
@@ -112,59 +83,23 @@ public class UserDbStorage extends BaseRepository<User> implements UserStorage {
         // Проверка существования пользователей
         log.info("Проверка существования пользователей: {} и {}", userId, friendId);
         User user = findById(userId).orElseThrow(() -> new NotFoundException("Пользователь с id " + userId + " не найден"));
-//        if (findById(userId).isEmpty()) {
-//            throw new NotFoundException("Пользователь с id " + userId + " не найден");
-//        }
-//        User friend = findById(friendId).orElseThrow(() -> new NotFoundException("Пользователь с id " + friendId + " не найден"));
         if (findById(friendId).isEmpty()) {
             throw new NotFoundException("Пользователь с id " + friendId + " не найден");
         }
         String INSERT_FRIEND_QUERY = "INSERT INTO FRIENDSHIP (USER_ID, FRIEND_ID) VALUES (?, ?)";
         insert(INSERT_FRIEND_QUERY, userId, friendId);
         user.setFriends(getFriendsSet(userId));
-
-        // Проверка статуса дружбы
-//        String FRIEND_CHECK_QUERY = "SELECT COUNT(*) FROM FRIENDSHIP WHERE USER_ID = ? AND FRIEND_ID = ?";
-//        findOne(FRIEND_CHECK_QUERY, )
-
-//        Boolean status = jdbc.queryForObject(FRIEND_CHECK_QUERY, Boolean.class, friendId, userId);
-//        addFriendship(friendId, userId);
-
-        // Запросы на добавление или обновление дружбы
-//        if (status == null) {
-//            addFriendship(userId, friendId, false);
-//
-//            addFriendship(friendId, userId, true);
-//        } else if (Boolean.TRUE.equals(status)) {
-//            updateFriendshipStatus(userId, friendId);
-//        }
-
         log.info("Пользователь с id {} добавил в друзья пользователя с id {}.", userId, friendId);
     }
 
-//    private boolean isUserExist(Long userId) {
-//        return findById(userId).isPresent();
-//    }
-
-//    private void addFriendship(Long userId, Long friendId) {
-//        String INSERT_FRIEND_QUERY = "INSERT INTO FRIENDSHIP (USER_ID, FRIEND_ID) VALUES (?, ?)";
-//        insert(INSERT_FRIEND_QUERY, friendId, userId);
-//    }
-
-//    private void updateFriendshipStatus(Long userId, Long friendId) {
-//        String UPDATE_STATUS_QUERY = "UPDATE FRIENDSHIP SET STATUS = ? WHERE USER_ID = ? AND FRIEND_ID = ?";
-//        insert(UPDATE_STATUS_QUERY, true, userId, friendId);
-//    }
-
     public List<User> getUserFriends(Long id) {
-        User user = findById(id).orElseThrow(() -> new NotFoundException("Пользователь с id " + id + " не найден"));
-        user.setFriends(getFriendsSet(id));
-        System.out.println(user);
-        return findMany(FIND_FRIENDS_BY_ID, id);
+        List<User> friends = findMany(FIND_FRIENDS_BY_ID, id);
+        for (User friend : friends) {
+            friend.setFriends(getFriendsSet(friend.getId()));
+        }
+        return friends;
+
     }
-//    public List<Long> getUserFriends(Long id) {
-//        return findManyIds(FIND_FRIENDS_BY_ID, id);
-//    }
 
     //Вспомогательный метод для получения коллекции друзей пользователя
     private Set<Long> getFriendsSet(Long id) {
@@ -179,9 +114,14 @@ public class UserDbStorage extends BaseRepository<User> implements UserStorage {
 //            throw new NotFoundException("Пользователь с id " + userId + " не найден");
 //        }
 //
-        if (findById(friendId).isEmpty()) {
-            throw new NotFoundException("Пользователь с id " + friendId + " не найден");
+        String FIND_FRIEND_STATUS_QUERY = "SELECT COUNT(*) FROM FRIENDSHIP WHERE USER_ID = ? AND FRIEND_ID = ?";
+        int friendshipStatus = jdbc.queryForObject(FIND_FRIEND_STATUS_QUERY, Integer.class, userId, friendId);
+        if (friendshipStatus == 0) {
+            throw new NotFoundException("Пользователь с id " + userId + " не является другом для пользователя с id " + friendId);
         }
+//        if (findById(friendId).isEmpty()) {
+//            throw new NotFoundException("Пользователь с id " + friendId + " не найден");
+//        }
         String DELETE_FRIEND_QUERY = "DELETE FROM FRIENDSHIP WHERE USER_ID = ? AND FRIEND_ID = ?";
         update(DELETE_FRIEND_QUERY, userId, friendId);
         user.setFriends(getFriendsSet(userId));
