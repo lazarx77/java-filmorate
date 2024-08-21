28a47bb0064fa5041201a8411042acd47b0b27fa
Dmitiy Stepanov <ariorxis@gmail.com>
2024-08-21 13:48:58 +0400

    fix/ update checkstyle

--- src/main/java/ru/yandex/practicum/filmorate/service/FeedService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FeedService.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dal.HistoryDbStorage;
-import ru.yandex.practicum.filmorate.dal.UserDbStorage;
 import ru.yandex.practicum.filmorate.model.Event;
 
 import java.util.*;
