c569e1372ebeda15b73f0ce8c91ea1e9418b02ef
Ultro163 <161496072+Ultro163@users.noreply.github.com>
2024-08-24 12:34:29 +0400

    Исправлены ошибки

--- src/main/java/ru/yandex/practicum/filmorate/service/FeedService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/FeedService.java
@@ -3,6 +3,7 @@ package ru.yandex.practicum.filmorate.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.dal.HistoryDbStorage;
+import ru.yandex.practicum.filmorate.dal.UserDbStorage;
 import ru.yandex.practicum.filmorate.model.Event;
 
 import java.util.ArrayList;
@@ -13,8 +14,10 @@ import java.util.List;
 public class FeedService {
 
     private final HistoryDbStorage historyDbStorage;
+    private final UserDbStorage userDbStorage;
 
     public List<Event> getFeed(long userId) {
+        userDbStorage.getUserById(userId);
         return new ArrayList<>(historyDbStorage.getEventsByUser(userId));
     }
 }
